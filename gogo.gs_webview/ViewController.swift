a8d65fd39e89a1cd84c3c92632da27e720fd3cd8
hogehogeBrav <zmzmakoto@icloud.com>
2022-06-03 05:10:28 +0900

    first commit

--- gogo.gs_webview/ViewController.swift
+++ gogo.gs_webview/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITabBarDelegate{
 
+  @IBOutlet weak var webview: WKWebView!
+  @IBOutlet weak var tabbar: UITabBar!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    tabbar.delegate = self
     // Do any additional setup after loading the view.
+    let url = URL(string: "https://gogo.gs/")
+    let request = URLRequest(url: url!)
+    webview.load(request)
   }
-
-
+  
+  @IBAction func backButton(_ sender: Any) {
+    if(webview.canGoBack){
+        webview.goBack()
+    }
+  }
+  
+  @IBAction func forwardButton(_ sender: Any) {
+    if(webview.canGoForward){
+        webview.goForward()
+    }
+  }
+  
+  
+  func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+         switch item.tag {
+         case 0:
+           let url = URL(string: "https://gogo.gs/")
+           let request = URLRequest(url: url!)
+           webview.load(request)
+         case 1:
+           let url = URL(string: "https://gogo.gs/ranking/average/")
+           let request = URLRequest(url: url!)
+           webview.load(request)
+         case 2:
+           let url = URL(string: "https://gogo.gs/chart/")
+           let request = URLRequest(url: url!)
+           webview.load(request)
+         case 3:
+           let url = URL(string: "https://gogo.gs/nearby/")
+           let request = URLRequest(url: url!)
+           webview.load(request)
+         case 4:
+           let url = URL(string: "https://gogo.gs/map/?gps=1")
+           let request = URLRequest(url: url!)
+           webview.load(request)
+         default:break
+         }
+  }
 }
-
