240929ce1139507c315cb1de73cc09f2d814dee1
hogehogeBrav <zmzmakoto@icloud.com>
2022-06-03 04:02:15 +0900

    Initial Commit

---
+++ gogo.gs_webview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gogo.gs_webview
+//
+//  Created by Makoto Miyahara on 2022/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
